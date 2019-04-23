544d09f38c5dcf3566dc31860b1db2588fde877a
Yauheni1988 <stigmat19@yandex.ru>
2019-04-23 20:41:26 +0300

    Added logic to VC

--- git/ViewController.swift
+++ git/ViewController.swift
@@ -12,6 +12,8 @@ class ViewController: UIViewController {
 
   override func viewDidLoad() {
     super.viewDidLoad()
+    
+    print("load");
     // Do any additional setup after loading the view.
   }
 
